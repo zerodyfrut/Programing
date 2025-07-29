176b52427c55905ddf883f3a84c9227de625f3ff
sol <zerodyfrut@gmail.com>
2025-07-29 18:18:52 +0900

    day0729 18:18

---
+++ Academy/SpringFramework/day0724-25,28-29/julspringjpa/src/test/java/com/julspringjpa/JulspringjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulspringjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
