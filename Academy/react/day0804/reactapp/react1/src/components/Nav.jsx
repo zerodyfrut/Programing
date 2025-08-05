883b89b24c9bf3a332001db9f33aed63d864258c
sol <zerodyfrut@gmail.com>
2025-08-05 18:10:31 +0900

    day0805 18:11

--- Academy/react/day0804/reactapp/react1/src/components/Nav.jsx
+++ Academy/react/day0804/reactapp/react1/src/components/Nav.jsx
@@ -3,7 +3,7 @@ function Nav(props) {
 
     for (let i = 0; i < props.topics.length; i++) {
         let t = props.topics[i]
-        lis.push(<li key={t.id}>{t.title} {t.body}</li>);
+        lis.push(<li key={t.id}>{t.title} : {t.body}</li>);
         
     }
 
