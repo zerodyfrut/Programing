b9eaa34a616959ad58279f2e1677d20f7eec94d0
sol <zerodyfrut@gmail.com>
2025-09-08 18:03:27 +0900

    day0908 18:03

---
+++ Academy/Python/day0908/login_book/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column,Integer,String,ForeignKey,Numeric
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__='users'
+    id=Column(Integer,primary_key=True,index=True)
+    username=Column(String(50),unique=True,nullable=False,index=True)
+    hashed_password=Column(String(255),nullable=False) 
+    # 해시화된 암호 : 길이가 길어지니 충분한 길이로
+    items=relationship("Item",back_populates="owner",cascade="all,delete-orphan")
+
+class Item(Base):
+    __tablename__='items'
+    id=Column(Integer,primary_key=True,index=True)
+    name=Column(String(255),nullable=False)
+    price=Column(Numeric(10,2),nullable=False)
+    is_offer=Column(String(5),nullable=True) # "true"/None (간단화)
+    owner_id=Column(Integer,ForeignKey("users.id"),nullable=False)
+
+    owner=relationship("User",back_populates="items")
+
+   
